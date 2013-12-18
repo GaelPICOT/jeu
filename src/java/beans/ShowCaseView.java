d6fcc388b6a5db5c2bd936cbce2e5a693a6879cd
xc1427 <xichen1427@gmail.com>
2013-12-18 17:15:53 +0100

    dirty implementation of 1. menu ;  2. showcase. test of showcase can be done without database..

--- src/java/beans/ShowCaseView.java
+++ src/java/beans/ShowCaseView.java
@@ -35,7 +35,46 @@ public class ShowCaseView {
     }
     
     public void initShowCase() {
-        this.showcase = product2Facade.findAll();
+        //this.showcase = product2Facade.findAll();
+        
+        // test sans base de donne
+        Product2 p1 = new Product2();
+        
+        p1.setName("produit1");
+        p1.setDescription("description jolie de 1");
+        p1.setImage("img/prima1.jpg");
+        p1.setPrice(10);
+        p1.setReduction(1);
+        
+        Product2 p2 = new Product2();
+        
+        p2.setName("produit2");
+        p2.setDescription("description jolie de 2");
+        p2.setImage("img/prima2.jpg");
+        p2.setPrice(20);
+        p2.setReduction(2);
+        
+        Product2 p3 = new Product2();
+        
+        p3.setName("produit3");
+        p3.setDescription("description jolie de 3");
+        p3.setImage("img/prima3.jpg");
+        p3.setPrice(30);
+        p2.setReduction(3);
+        
+        Product2 p4= new Product2();
+        
+        p4.setName("produit4");
+        p4.setDescription("description jolie de 4");
+        p4.setImage("img/prima4.jpg");
+        p4.setPrice(40);
+        p4.setReduction(4);
+        
+        this.showcase.add(p1);
+        this.showcase.add(p2);
+        this.showcase.add(p3);
+        this.showcase.add(p4);
+                
         
     }
     
