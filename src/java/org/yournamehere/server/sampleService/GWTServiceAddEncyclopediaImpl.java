4df0dddbdbf01c1a94a7945354c76b2e0c3ae0dd
Gaël PICOT <gael.picot@free.fr>
2013-12-18 16:31:37 +0100

    Ajout dans l'encyclopedie corriger (Non recursif)

--- src/java/org/yournamehere/server/sampleService/GWTServiceAddEncyclopediaImpl.java
+++ src/java/org/yournamehere/server/sampleService/GWTServiceAddEncyclopediaImpl.java
@@ -86,73 +86,83 @@ public class GWTServiceAddEncyclopediaImpl extends RemoteServiceServlet implemen
     
     @Override
     public String createEncyclopediaNode(SemanticNode nodeAdd) {
+        marche (nodeAdd);
+          
+        return "";
+    }
+    
+    public void marche (SemanticNode nodeAdd) {
         //On crée un objet Class correspondant a la class du bean a traiter
         Class beanClass = nodeAdd.getClass();
+        if(nodeAdd.getId()==null) {
         
-        for (Method meth : beanClass.getMethods()) {
-            if (meth.getReturnType().isAssignableFrom(nodeAdd.getClass()) && meth.getParameterTypes().length==0) {
-                try {
-                    createEncyclopediaNode((SemanticNode) meth.invoke(nodeAdd));
-                } catch (IllegalAccessException ex) {
-                    Logger.getLogger(GWTServiceAddEncyclopediaImpl.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IllegalArgumentException ex) {
-                    Logger.getLogger(GWTServiceAddEncyclopediaImpl.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (InvocationTargetException ex) {
-                    Logger.getLogger(GWTServiceAddEncyclopediaImpl.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-            if(meth.getReturnType().isAssignableFrom(List.class) && meth.getParameterTypes().length==0) {
-                try {
-                    List laList = (List)meth.invoke(nodeAdd);
-                    if (laList.get(0).getClass().isAssignableFrom(SemanticNode.class)) {
-                        for (Object SN : laList) {
-                            createEncyclopediaNode((SemanticNode) SN);
-                        }
-                    }
-                } catch (IllegalAccessException ex) {
-                    Logger.getLogger(GWTServiceAddEncyclopediaImpl.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IllegalArgumentException ex) {
-                    Logger.getLogger(GWTServiceAddEncyclopediaImpl.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (InvocationTargetException ex) {
-                    Logger.getLogger(GWTServiceAddEncyclopediaImpl.class.getName()).log(Level.SEVERE, null, ex);
-                }
+//            for (Method meth : beanClass.getMethods()) {
+//                if (meth.getReturnType().isAssignableFrom(nodeAdd.getClass()) && meth.getParameterTypes().length==0) {
+//                    try {
+//                        SemanticNode SN = (SemanticNode) meth.invoke(nodeAdd);
+//                        if(SN!=null)
+//                            marche(SN);
+//                    } catch (IllegalAccessException ex) {
+//                        Logger.getLogger(GWTServiceAddEncyclopediaImpl.class.getName()).log(Level.SEVERE, null, ex);
+//                    } catch (IllegalArgumentException ex) {
+//                        Logger.getLogger(GWTServiceAddEncyclopediaImpl.class.getName()).log(Level.SEVERE, null, ex);
+//                    } catch (InvocationTargetException ex) {
+//                        Logger.getLogger(GWTServiceAddEncyclopediaImpl.class.getName()).log(Level.SEVERE, null, ex);
+//                    }
+//                }
+//                if(meth.getReturnType().isAssignableFrom(List.class) && meth.getParameterTypes().length==0) {
+//                    try {
+//                        List laList = (List)meth.invoke(nodeAdd);
+//                        if (laList.get(0).getClass().isAssignableFrom(SemanticNode.class)) {
+//                            if (laList!=null)
+//                                for (Object SN : laList) {
+//                                    if(SN!=null)
+//                                        marche((SemanticNode) SN);
+//                                }
+//                        }
+//                    } catch (IllegalAccessException ex) {
+//                        Logger.getLogger(GWTServiceAddEncyclopediaImpl.class.getName()).log(Level.SEVERE, null, ex);
+//                    } catch (IllegalArgumentException ex) {
+//                        Logger.getLogger(GWTServiceAddEncyclopediaImpl.class.getName()).log(Level.SEVERE, null, ex);
+//                    } catch (InvocationTargetException ex) {
+//                        Logger.getLogger(GWTServiceAddEncyclopediaImpl.class.getName()).log(Level.SEVERE, null, ex);
+//                    }
+//                }
+//            }
+
+            if (beanClass.getName().equals(Accessory.class.getName())) {
+                accessoryFacade.create((Accessory)nodeAdd);
+            } else if (beanClass.getName().equals(Actor.class.getName())) {
+                actorFacade.create((Actor)nodeAdd);
+            } else if (beanClass.getName().equals(Article.class.getName())) {
+                articleFacade.create((Article)nodeAdd);
+            } else if (beanClass.getName().equals(Association.class.getName())) {
+                associationFacade.create((Association)nodeAdd);
+            } else if (beanClass.getName().equals(Book.class.getName())) {
+                bookFacade.create((Book)nodeAdd);
+            } else if (beanClass.getName().equals(Category.class.getName())) {
+                categoryFacade.create((Category)nodeAdd);
+            } else if (beanClass.getName().equals(Entreprise.class.getName())) {
+                entrepriseFacade.create((Entreprise)nodeAdd);
+            } else if (beanClass.getName().equals(Game.class.getName())) {
+                gameFacade.create((Game)nodeAdd);
+            } else if (beanClass.getName().equals(Image.class.getName())) {
+                imageFacade.create((Image)nodeAdd);
+            } else if (beanClass.getName().equals(Licence.class.getName())) {
+                licenceFacade.create((Licence)nodeAdd);
+            } else if (beanClass.getName().equals(Person.class.getName())) {
+                personFacade.create((Person)nodeAdd);
+            } else if (beanClass.getName().equals(Productible.class.getName())) {
+                productibleFacade.create((Productible)nodeAdd);
+            } else if (beanClass.getName().equals(Release.class.getName())) {
+                releaseFacade.create((Release)nodeAdd);
+            } else if (beanClass.getName().equals(Rule.class.getName())) {
+                ruleFacade.create((Rule)nodeAdd);
+            } else if (beanClass.getName().equals(Theme.class.getName())) {
+                themeFacade.create((Theme)nodeAdd);
+            } else if (beanClass.getName().equals(Person.class.getName())) {
+               personFacade.create((Person)nodeAdd);
             }
         }
-        
-        if (beanClass.getName().equals(Accessory.class.getName())) {
-            accessoryFacade.create((Accessory)nodeAdd);
-        } else if (beanClass.getName().equals(Actor.class.getName())) {
-            actorFacade.create((Actor)nodeAdd);
-        } else if (beanClass.getName().equals(Article.class.getName())) {
-            articleFacade.create((Article)nodeAdd);
-        } else if (beanClass.getName().equals(Association.class.getName())) {
-            associationFacade.create((Association)nodeAdd);
-        } else if (beanClass.getName().equals(Book.class.getName())) {
-            bookFacade.create((Book)nodeAdd);
-        } else if (beanClass.getName().equals(Category.class.getName())) {
-            categoryFacade.create((Category)nodeAdd);
-        } else if (beanClass.getName().equals(Entreprise.class.getName())) {
-            entrepriseFacade.create((Entreprise)nodeAdd);
-        } else if (beanClass.getName().equals(Game.class.getName())) {
-            gameFacade.create((Game)nodeAdd);
-        } else if (beanClass.getName().equals(Image.class.getName())) {
-            imageFacade.create((Image)nodeAdd);
-        } else if (beanClass.getName().equals(Licence.class.getName())) {
-            licenceFacade.create((Licence)nodeAdd);
-        } else if (beanClass.getName().equals(Person.class.getName())) {
-            personFacade.create((Person)nodeAdd);
-        } else if (beanClass.getName().equals(Productible.class.getName())) {
-            productibleFacade.create((Productible)nodeAdd);
-        } else if (beanClass.getName().equals(Release.class.getName())) {
-            releaseFacade.create((Release)nodeAdd);
-        } else if (beanClass.getName().equals(Rule.class.getName())) {
-            ruleFacade.create((Rule)nodeAdd);
-        } else if (beanClass.getName().equals(Theme.class.getName())) {
-            themeFacade.create((Theme)nodeAdd);
-        } else if (beanClass.getName().equals(Person.class.getName())) {
-           personFacade.create((Person)nodeAdd);
-        }
-          
-        return nodeAdd.getClass().getName();
     }
 }
